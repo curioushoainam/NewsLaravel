57449e1dc9fad9ad040aee55113192b391df1ab5
curioushoainam <hoainamtruongute@gmail.com>
2018-09-18 22:57:36 +0700

    make the slide from database

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\TheLoai;
+use \App\Slide;
 
 class PagesController extends Controller
 {
     
 	function __construct(){
 		$theloai = TheLoai::all();
+		$slide = Slide::all();		
 		view()->share('theloai',$theloai);
+		view()->share('slide',$slide);
 	}
 
     public function getTrangchu(){    	    	
